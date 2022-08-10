788ac4838dae20dbf7e0bce2ee635ba7aa02b7e3
ritik8262 <ritiksingla8262@gmail.com>
2022-08-10 23:40:16 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,53 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const LOCALHOST_RPC_URL = process.env.LOCALHOST_RPC_URL
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+        mumbai: {
+            url: POLYGON_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 80001,
+        },
+        localhost: {
+            url: LOCALHOST_RPC_URL,
+            // accounts: Thanx Hardhat!
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.7",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
